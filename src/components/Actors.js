0019b35c3e4282c5dae75372f6c338960eb4ccec
Krae Wind <kraewind@gmail.com>
2021-07-14 13:17:57 -0400

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import { actors } from '../data';
 
+
+// debugger
+const makeMovies = (moviesList) => {
+  return (moviesList.map(movie => {
+    return <li>{movie}</li>
+  }))
+}
+
+const makeActors = () => {
+  // debugger
+  return actors.map(actor => {
+    return (<div>
+      Name: {actor.name}
+      Movies: 
+      <ul>
+      {makeMovies(actor.movies)}
+</ul>
+    </div>)
+  })
+}
+
+
 const Actors = () => {
+  // debugger
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {makeActors()}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import { directors } from '../data';
+import { directors, movies } from '../data';
+
+const makeMovies = (moviesList) => {
+  return moviesList.map(movie => {
+    return <li>{movie}</li>
+  })
+}
+
+const makeDirectors = () => {
+  return directors.map(director => {
+    return(<div>
+      Name: {director.name}
+      Movies: 
+      <ul>
+      {makeMovies(director.movies)}
+</ul>
+    </div>)
+  })
+}
 
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {makeDirectors()}
     </div>
   );
 }
