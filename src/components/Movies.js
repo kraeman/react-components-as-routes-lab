1fc5bbb1b6fb5e825b812f3dd0ea6bfa4efc3651
Krae Wind <kraewind@gmail.com>
2021-07-14 13:17:49 -0400

    commit tree 8ede3ae67bf1df79434eb4e95544fe40998194b6

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,31 @@
 import React from 'react';
 import { movies } from '../data';
 
+
+const makeGenres = (genresList) => {
+  return genresList.map(genre => {
+    return <li>{genre}</li>
+  })
+}
+
+const makeMovies = () => {
+  return movies.map(movie => {
+    return (<div>
+      Title: {movie.title}
+      Time: {movie.time}
+      Genres:
+      <ul>
+        {makeGenres(movie.genres)}
+      </ul>
+    </div>)
+  })
+}
+
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {makeMovies()}
     </div>
   );
 };
